6505835e3cdf2f6f15aeec1bce27274befa1e60b
benwend <benjamin.wend@free.fr>
2020-11-13 18:05:41 -0500

    up(php-fpm): Add task to install Phive

---
+++ roles/php-fpm/files/phive_install.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+# PHAR Installation and Verification Environment (PHIVE) installer
+# https://phar.io/
+#
+set -euo pipefail
+
+cd /tmp
+wget -O phive.phar https://phar.io/releases/phive.phar
+wget -O phive.phar.asc https://phar.io/releases/phive.phar.asc
+gpg --keyserver pool.sks-keyservers.net --recv-keys 0x9D8A98B29B2D5D79
+gpg --verify phive.phar.asc phive.phar
+
+chmod +x phive.phar
+mv phive.phar /usr/local/bin/phive
+
+rm phive.phar.asc
